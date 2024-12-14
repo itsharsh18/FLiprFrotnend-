c9727195e869f066bdd12e1e93f9b3c276a0f8db
itsharsh18 <itsharshjha18@gmail.com>
2024-12-14 13:17:56 +0530

    newly made cod.

--- src/Components/theFIrstSection/Formforclient.jsx
+++ src/Components/theFIrstSection/Formforclient.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import  Logo  from "../../assets/images/leadGEn/young-couple-examining-blueprints-with-real-estate-agent-while-buying-new-home 1.svg";
-import Secnd from '../theSecondComponent/secnd';
-import ThirdSectio from '../theThirdSection/ThirdSectio';
-import Projects from '../ourProject/Project';
-import Testimonials from '../HappyClients/Clients';
-import Last from '../LastSection/Last';
-import AdminRegisterForm from '../Admin/AdminRegisterForm';
+import Secnd from '../theSecondComponent/Secnd.jsx';
+import ThirdSectio from '../theThirdSection/ThirdSectio.jsx';
+import Projects from '../ourProject/Project.jsx';
+import Testimonials from '../HappyClients/Clients.jsx';
+import Last from '../LastSection/Last.jsx';
+import AdminRegisterForm from '../Admin/AdminRegisterForm.jsx';
 
 const Formforclient = () => {
 
